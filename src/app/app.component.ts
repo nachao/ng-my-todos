5fc6a55f171cd5a6acfb92e47f558ebf639f348f
Chao Na <357586693@qq.com>
2019-04-12 00:46:19 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-my-todos';
+
+  // 用于存储任务列表
+  list = [];
+
+  /**
+   * 数据处理事件 - 添加任务
+   * @param event
+   */
+  addTask (event) {
+
+    // 如果是回车键
+    // 则存储内容
+    if (event.code === 'Enter') {
+      this.list.push(event.target.value);
+
+      // 重置输入框
+      event.target.value = '';
+    }
+  }
+
+  /**
+   * 数据处理事件 - 添加任务
+   * @param item
+   */
+  delTask (item) {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
 }
