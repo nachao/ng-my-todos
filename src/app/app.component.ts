dc897e1beab45ce404f399e66d8aaa5e682043d6
nachao <357586693@qq.com>
2019-04-12 19:35:14 +0800

    feat: add form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,23 @@ export class AppComponent {
   // 用于存储任务列表
   list = [];
 
+  // 表单数据
+  editStatus = false
+  editItem = new FormGroup({
+    id: new FormControl(''),
+    title: new FormControl(''),
+    describe: new FormControl('')
+  });
+
+  // 数据
+  itemTask (id, title) {
+  	return {
+      id,
+  		title,
+  		describe: ''
+  	}
+  }
+
   /**
    * 数据处理事件 - 添加任务
    * @param event
@@ -19,7 +37,7 @@ export class AppComponent {
     // 如果是回车键
     // 则存储内容
     if (event.code === 'Enter') {
-      this.list.push(event.target.value);
+      this.list.push(this.itemTask(this.list.length + 1, event.target.value));
 
       // 重置输入框
       event.target.value = '';
@@ -32,5 +50,22 @@ export class AppComponent {
    */
   delTask (item) {
     this.list.splice(this.list.indexOf(item), 1);
+  }
+
+  editTask (item) {
+    this.editStatus = true
+    this.editItem.patchValue(item)
+  }
+
+  editTaskSave () {
+    console.log(this.editItem)
+    this.editStatus = false
+    const data = this.editItem.value
+    const item = this.list.find(d => d.id === data.id)
+    Object.assign(item, data)
+  }
+
+  editTaskQuit () {
+    this.editStatus = false
   }
 }
